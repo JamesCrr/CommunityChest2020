b32616007394a8a84202983758acfe93e178233e
Wx <g0546699m@gmail.com>
2020-02-14 19:22:40 +0800

    - Trying out Unity TileMap Systems and their prefab brushes

---
+++ ComChest_Unity/Assets/Scripts/Testing/TestPlace.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.Tilemaps;
+using UnityEngine.Tilemaps;
+
+public class TestPlace : MonoBehaviour
+{
+    public GridLayout gridLayout = null;
+    public Tilemap targetTileMap = null;
+    public GameObject targetToPaint = null;
+    [SerializeField]
+    PrefabBrush pondBrush = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonUp(0))
+        {
+            Vector3 worldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            worldPos.z = 0.0f;
+            Vector3Int gridPos = targetTileMap.WorldToCell(worldPos);
+            pondBrush.PlaceGO(targetTileMap, targetTileMap.gameObject, gridPos, 0);
+
+            GameObject newObject = pondBrush.GetGOInCell(targetTileMap, targetTileMap.transform, gridPos);
+            Debug.Log(newObject.name);
+        }
+
+    }
+}
