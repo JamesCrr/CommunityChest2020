7c4357ab31a75c36957dfd057491daed61516ace
Angie <173123N@mymail.nyp.edu.sg>
2020-02-28 11:34:50 +0800

    - Added resources infomation for buildings - Game data Manager class

---
+++ ComChest_Unity/Assets/Scripts/Player/PlayerDataManager.cs
@@ -0,0 +1,12 @@
+﻿
+public class PlayerDataManager : SingletonBase<PlayerDataManager>
+{
+    int m_TotalMoney = 0;
+    int m_TotalPopulation = 0;
+
+    // Start is called before the first frame update
+    public override void Awake()
+    {
+        //TODO init from save file
+    }
+}
