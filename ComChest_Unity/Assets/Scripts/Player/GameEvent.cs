2a0872c487be61abbc0858c73bc8f30b46987ff0
gideon <gideonnfrancis999@gmail.com>
2020-04-10 21:48:44 +0800

    Testing events

---
+++ ComChest_Unity/Assets/Scripts/Player/GameEvent.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Events")]
+public class GameEvent : ScriptableObject
+{
+    [Header("Event Data")]
+    [SerializeField]
+    string m_EventName = "Unnamed Event";
+    
+
+}
--- ComChest_Unity/Assets/Scripts/Player/PlayerDataManager.cs
+++ ComChest_Unity/Assets/Scripts/Player/PlayerDataManager.cs
@@ -3,6 +3,13 @@ public class PlayerDataManager : SingletonBase<PlayerDataManager>
 {
     int m_TotalMoney = 0;
     int m_TotalPopulation = 0;
+    int m_TotalHappiness = 0;
+    int m_TotalElectricity = 0;
+    int m_TotalWater = 0;
+
+    int m_RequiredWater = 0;
+    int m_RequiredElectricity = 0;
+    
 
     // Start is called before the first frame update
     public override void Awake()
@@ -18,7 +25,26 @@ public class PlayerDataManager : SingletonBase<PlayerDataManager>
 
     public int GetSetTotalPopulation
     {
-        get { return GetSetTotalPopulation; }
-        set { GetSetTotalPopulation = value; }
+        get { return m_TotalPopulation; }
+        set { m_TotalPopulation = value; }
     }
+
+    public int GetSetTotalHappiness
+    {
+        get { return m_TotalHappiness; }
+        set { m_TotalHappiness = value; }
+    }
+
+    public int GetSetTotalElectricity
+    {
+        get { return m_TotalElectricity; }
+        set { m_TotalElectricity = value; }
+    }
+
+    public int GetSetTotalWater
+    {
+        get { return m_TotalWater; }
+        set { m_TotalWater = value; }
+    }
+
 }
