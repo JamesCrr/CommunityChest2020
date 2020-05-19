eb97f01a75e5e8e3222e63d22c87e76dfa4ab468
Angie <173123N@mymail.nyp.edu.sg>
2020-05-20 02:17:17 +0800

    - Shop checks whether player has enough money to buy the resources or not now

--- ComChest_Unity/Assets/Scripts/UI/Ingame/Shop/ShopItemCardUI.cs
+++ ComChest_Unity/Assets/Scripts/UI/Ingame/Shop/ShopItemCardUI.cs
@@ -11,18 +11,24 @@ public class ShopItemCardUI : MonoBehaviour
     [SerializeField] Image m_ItemImage;
 
     BuildingDataBase.BUILDINGS m_BuildingType;
-    bool m_Unlocked = false;
+    bool m_Buyable = false; //check if player has the resources needed to build this
+    Animator m_Animator;
 
-    // Start is called before the first frame update
-    void Start()
+    public void Awake()
     {
-        //check whether player unlocked this or not
+        m_Animator = GetComponent<Animator>();
     }
 
     private void OnEnable()
     {
         //check whether player is able to purchase this item
         //update the UI accordingly
+        m_Buyable = ResourceManager.GetInstance().EnoughResourcesForBuilding(m_BuildingType);
+
+        if (m_Buyable)
+            m_PriceText.color = Color.white;
+        else
+            m_PriceText.color = Color.red;
     }
 
     public void Init(BuildingData m_BuildingData)
@@ -46,8 +52,18 @@ public class ShopItemCardUI : MonoBehaviour
         if (IngameUIManager.instance == null)
             return;
 
-        //close shop and activate UI for placement
-        IngameUIManager.instance.SetShopMenuActive(false);
-        IngameUIManager.instance.PlayerInBuildModeUI(true, m_BuildingType);
+        //check whether player has money, if dont, show a UI and animate
+        if (m_Buyable)
+        {
+            //close shop and activate UI for placement
+            IngameUIManager.instance.SetShopMenuActive(false);
+            IngameUIManager.instance.PlayerInBuildModeUI(true, m_BuildingType);
+        }
+        else
+        {
+            //activate animation
+            if (m_Animator != null)
+                m_Animator.SetTrigger("Shake");
+        }
     }
 }
