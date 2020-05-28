45d9ca1ff608c7c5ca3058b961e7b9afdcbe2333
gideon <gideonnfrancis999@gmail.com>
2020-05-28 16:30:05 +0800

    :cry: :cry: :dog2: :car: :fireworks:

--- ComChest_Unity/Assets/Scripts/UI/BuildingUI/CC_UIManager.cs
+++ ComChest_Unity/Assets/Scripts/UI/BuildingUI/CC_UIManager.cs
@@ -20,4 +20,19 @@ public class CC_UIManager : MonoBehaviour
     {
         IngameUIManager.instance.CloseBuildingUI();
     }
+
+    public void OpenFacebook()
+    {
+        Application.OpenURL("https://www.facebook.com/comchest/");
+    }
+
+    public void OpenInstagram()
+    {
+        Application.OpenURL("https://www.instagram.com/comchestsg/");
+    }
+
+    public void OpenTwitter()
+    {
+        Application.OpenURL("https://twitter.com/comchestsg");
+    }
 }
